d8758b8662eeff5fb344f17b54737491ad100051
loczippo <kimloc1000@gmail.com>
2022-06-07 00:33:05 +0700

    asd

--- js/auth.js
+++ js/auth.js
@@ -15,14 +15,11 @@ if (getAppToken() !== null) {
   );
 }
 
-$('#login-button').click(function (event) {
-  event.preventDefault();
-
   $('.wrapper').addClass('form-success');
   $('form').fadeOut(500);
   $('#loading').fadeIn(500);
-  
-  var host = $('#host').val() || 'https://svchatbot.tk';
+
+var host = 'https://newchatible.herokuapp.com';
   setAppUrl(host);
   var d = new Date();
   var time = d.getTime();
@@ -53,4 +50,43 @@ $('#login-button').click(function (event) {
       }
     );
   }, 500);
-});
+
+// $('#login-button').click(function (event) {
+//   event.preventDefault();
+
+//   $('.wrapper').addClass('form-success');
+//   $('form').fadeOut(500);
+//   $('#loading').fadeIn(500);
+  
+//   var host = 'https://newchatible.herokuapp.com';
+//   setAppUrl(host);
+//   var d = new Date();
+//   var time = d.getTime();
+//   var pwd = CryptoJS.SHA256('loczippo').toString();
+//   var token = {
+//     time: time,
+//     hash: CryptoJS.SHA256(time + '' + pwd).toString()
+//   };
+//   token = CryptoJS.AES.encrypt(JSON.stringify(token), pwd).toString();
+//   setTimeout(function () {
+//     checkToken(
+//       token,
+//       function (res) {
+//         if (res.success === true) {
+//           setAppToken(token);
+//           window.location.href = '/admin.html';
+//         } else if (res.error === true && res.errortype === 'auth') {
+//           alert('Sai mật khẩu!');
+//           redirectToLogin();
+//         } else {
+//           alert('Có lỗi xảy ra!');
+//           redirectToLogin();
+//         }
+//       },
+//       function () {
+//         alert('Lỗi kết nối!');
+//         redirectToLogin();
+//       }
+//     );
+//   }, 500);
+// });
